b6a047e42a160f68ba3ce34d8a6a13ce6940ab5a
iustina-d <iustina2dulea@gmail.com>
2023-02-13 23:36:41 +0200

    get new data

---
+++ myproject/myproject.py
@@ -0,0 +1,20 @@
+# import os
+# from flask import Flask,render_template
+# app = Flask(__name__)
+
+# @app.route("/")
+# def main():
+#   return render_template("home.html")
+
+
+# if __name__ == "__main__":
+#     app.run()
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return "<h1 style='color:blue'>Hello There!</h1>"
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0')
